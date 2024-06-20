b33066157284101dff442c66c3ba5208e3460056
framallo <ramallofranco17@gmail.com>
2024-06-20 16:51:35 -0300

    Ejercicio 1 del TP2

---
+++ TP2/EJ1/main.js
@@ -0,0 +1,12 @@
+let btn1 = document.getElementById("boton1");
+let btn2 = document.getElementById("boton2");
+let btn3 = document.getElementById("boton3");
+let mensaje = document.getElementById("mensaje");
+
+btn1.addEventListener("click", () => aviso("Azul"));
+btn2.addEventListener("click", () => aviso("Amarillo"));
+btn3.addEventListener("click", () => aviso("Rojo"));
+
+function aviso(color) {
+    mensaje.innerHTML = "El último botón clickeado fue el " + color;
+}
