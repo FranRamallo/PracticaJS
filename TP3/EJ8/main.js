ba7b2c77e88d2064723ed05c1c0ed73d15fd8503
framallo <ramallofranco17@gmail.com>
2024-07-13 11:00:47 -0300

    Ejercicio 8 del TP3

---
+++ TP3/EJ8/main.js
@@ -0,0 +1,9 @@
+document.getElementById("borrar").addEventListener("click", () => {
+    let parrafos = document.querySelectorAll("p");
+    if (parrafos.length > 0) {
+        let ultimoParrafo = parrafos[parrafos.length - 1];
+        ultimoParrafo.remove();
+    } else {
+        alert("No hay más párrafos para borrar.");
+    }
+});
